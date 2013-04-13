b589d9c6077bb2a13d3d250996e0d8d8f7be59f5
skinnerj@goldmail.etsu.edu <skinnerj@goldmail.etsu.edu>
2013-04-13 10:53:47 -0400

    initial commit

---
+++ blogger/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '0166fba1119e935c18e84a45f1d573df59b42138c23822701a7e30429a6d856adba42305842ecc04fab6af6e22cd89a387837b0f13ccb5720fd747138540ac28'
